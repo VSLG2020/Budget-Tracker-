2c8e7c42b35c21ce2f99208c3bd6e6430d8a1d22
Vanessa G <vlopez_jv@hotmail.com>
2021-04-13 23:51:55 -0700

    added manifest.json and finished servcwrkr

--- public/service-worker.js
+++ public/service-worker.js
@@ -23,3 +23,72 @@ const FILES_TO_CACHE = [
 self.addEventListener('install', function (e) {
 
 })
+
+//Add the event waitUntil() function shown in the following code as the callback function of the install event listener:
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+})
+
+// Activate service worker and delete old data from cache
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+      caches.keys().then(function (keyList) {
+        let cacheKeeplist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX);
+        });
+        cacheKeeplist.push(CACHE_NAME);
+  
+        return Promise.all(
+          keyList.map(function (key, i) {
+            if (cacheKeeplist.indexOf(key) === -1) {
+              console.log('deleting cache : ' + keyList[i]);
+              return caches.delete(keyList[i]);
+            }
+          })
+        );
+      })
+    );
+  });
+  
+  // get fetch requests
+  self.addEventListener('fetch', function (event) {
+    
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches
+        .open(VERSION)
+        .then(cache => {
+            return fetch(event.request)
+              .then(response => {
+                if (response.status === 200){
+                  cache.put(event.request.url, response.clone())
+                }
+                return response
+              })
+              .catch(err => {
+                return cache.match(event.request); 
+              })
+          })
+          .catch(err => console.log(err))
+      ) 
+      return;
+    }
+    event.respondWith(
+        fetch(event.request).catch(function () {
+            return caches.match(event.request).then(function (response) {
+                if (response) {
+                    return response;
+                } else if (event.request.headers.get('accept').includes('text/html')) {
+                    // return the cached home page for all requests for html pages
+                    return caches.match('/');
+                }
+            });
+        })
+    );
+});
