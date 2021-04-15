49b37aad01f97af425d91b4995362242a5766e8d
Vanessa G <vlopez_jv@hotmail.com>
2021-04-14 23:06:02 -0700

    working service-worker

--- public/js/idb.js
+++ public/js/idb.js
@@ -3,22 +3,23 @@ let db;
 // establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
 const request = indexedDB.open('budget_tracker', 1);
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+
 request.onupgradeneeded = function (event) {
-    // save a reference to the database 
+    // save a reference to the database
     const db = event.target.result;
-    // create an object store (table) called `newBudgetEntry`, set it to have an auto incrementing primary key of sorts 
+    // create an object store called `newBudgetEntry`, set it to have an auto incrementing primary key of sorts 
     db.createObjectStore('newBudgetEntry', { autoIncrement: true });
 };
 
 // upon a successful 
 request.onsuccess = function (event) {
-    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    // when db is successfully created save to global 'db' variable
     db = event.target.result;
 
-    // check if app is online, if yes run uploadAction() function to send all local db data to api
+    // check if app is online, if yes run uploadAction() function to send all local db data to api   
     if (navigator.onLine) {
-        
-        uploadAction();
+
+        uploadBudget();
     }
 };
 
@@ -28,34 +29,34 @@ request.onerror = function (event) {
 };
 
 // This function will be executed if we attempt to submit a new pizza and there's no internet connection
+
+// if there is no internet connection this function will execute if a budget is submitted
 function saveRecord(record) {
-    // open a new transaction with the database with read and write permissions 
+    // open a new transaction with the database with read and write permissions
     const transaction = db.transaction(['newBudgetEntry'], 'readwrite');
 
-    // access the object store for `newBudgetEntry`
-    const storeEntry = transaction.objectStore('newBudgetEntry');
+    // access the object store for 'newBudgetEntry'
+    const storeObjectEntry = transaction.objectStore('newBudgetEntry');
 
     // add record to your store with add method
-    storeEntry.add(record);
+    storeObjectEntry.add(record);
 }
-
 // Create a function that will handle collecting all of the data from the newBudgetEntry object store in IndexedDB and POST it to the server
-function uploadAction() {
+function uploadBudget() {
     // Open a new transaction to the database to read the data.
     const transaction = db.transaction(['newBudgetEntry'], 'readwrite');
 
-    // access object store
-    const storeEntry = transaction.objectStore('newBudgetEntry');
+    // access the object store
+    const storeObjectEntry = transaction.objectStore('newBudgetEntry');
 
     // Get all records from store and set to variable
-    const getAll = storeEntry.getAll();
-
+    const getAll = storeObjectEntry.getAll();
 
     // upon a successful .getAll() execution, run this function
     getAll.onsuccess = function () {
-        // if there was data in indexedDb's store, let's send it to the api server
+        // if there was data in indexedDb's store, send it to the api
         if (getAll.result.length > 0) {
-            fetch('/api/transaction/bulk', {
+            fetch('/api/transaction/', {
                 method: 'POST',
                 body: JSON.stringify(getAll.result),
                 headers: {
@@ -71,20 +72,20 @@ function uploadAction() {
                     // open one more transaction
                     const transaction = db.transaction(['newBudgetEntry'], 'readwrite');
                     // access the newBudgetEntry object store
-                    const storeEntry = transaction.objectStore('newBudgetEntry');
+                    const storeObjectEntry = transaction.objectStore('newBudgetEntry');
                     // clear all items in your store
-                    storeEntry.clear();
+                    storeObjectEntry.clear();
 
-                    alert('All saved transaction has been submitted!');
+                    alert('All saved transactions have been submitted!');
                 })
                 .catch(err => {
                     console.log(err);
                 });
         }
     }
-};
+}
 
 // listen for app coming back online
 // window.addEventListener('online', uploadPizza);
 // Listen for if the app comes back online
-window.addEventListener('online', uploadAction);
+window.addEventListener('online', uploadBudget);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,83 +1,69 @@
 // const APP_PREFIX = 'FoodFest-';     
 // const VERSION = 'version_01';
 // const CACHE_NAME = APP_PREFIX + VERSION;
-const CACHE_NAME = 'budget_tracker';
+const APP_PREFIX = 'budget_tracker';
 const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
 
+// list of files we will be caching
 const FILES_TO_CACHE = [
     "/",
-    "/index.html",
-    "/js/index.js",
+    "./index.html",
+    "./js/index.js",
     "./js/idb.js",
-    "/css/styles.css"
-   
-];
+    "./css/styles.css",
+    "./icons/icon-192x192.png"
 
-
+]
 //Add the event waitUntil() function shown in the following code as the callback function of the install event listener:
-
+// install the service worker
 self.addEventListener('install', function (event) {
-    console.log('Service worker activate event!');
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME);
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  
+    //console.log('Service worker activate event!');
+    event.waitUntil(
+        // find a specific cache by name then add every file in the FILES_TO_CACHE array to the cache
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache: ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
 })
 
 // Activate service worker and delete old data from cache
 // Activate Service-Worker
 self.addEventListener('activate', function (event) {
-    event.waitUntil (
-        caches.keys().then(keyList => {
-            return Promise.all (
-                keyList.map(key => {
-                    if (CACHE_NAME.indexOf(VERSION) === -1) {
-                        console.log('Deleting old' + CACHE_NAME + 'cache data', key);
-                        return caches.delete(key);
+    event.waitUntil(
+        caches.keys().then(function (keyList) {
+            let keepList = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            keepList.push(CACHE_NAME);
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (keepList.indexOf(key) === -1) {
+                        console.log('Deleting old :' + keyList[i]);
+                        return caches.delete(keyList[i]);
                     }
                 })
-            );
+            )
         })
-    );
-    self.clients.claim();
-});
+    )
+})
+// get fetch requests
+self.addEventListener('fetch', function (event) {
 
-  // get fetch requests
-  self.addEventListener('fetch', function (event) {
-    
-    if (event.request.url.includes("/api/")) {
-      event.respondWith(
-        caches
-        .open(VERSION)
-        .then(cache => {
-            return fetch(event.request)
-              .then(response => {
-                if (response.status === 200){
-                  cache.put(event.request.url, response.clone())
-                }
-                return response
-              })
-              .catch(err => {
-                return cache.match(event.request); 
-              })
-          })
-          .catch(err => console.log(err))
-      ) 
-      return;
-    }
     event.respondWith(
-        fetch(event.request).catch(function () {
-            return caches.match(event.request).then(function (response) {
-                if (response) {
-                    return response;
-                } else if (event.request.headers.get('accept').includes('text/html')) {
-                    // return the cached home page for all requests for html pages
-                    return caches.match('/');
+
+        caches
+            .match(event.request)
+            .then(function (request) {
+
+                if (request) {
+                    return request;
+                } else {
+
+                    console.log('file is not cached, fetching: ' + event.request.url)
+                    return fetch(event.request)
                 }
-            });
-        })
-    );
-});
+            })
+    )
+})
